4697531c6ddb7a4160a3e1ada0229ac0fbf2b416
alex <alex.oliveiramartins05@gmail.com>
2024-09-29 14:43:00 -0300

    initial commit

---
+++ harrypotterdle/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+// import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1>not found!</h1>}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
