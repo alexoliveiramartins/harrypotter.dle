5444c674156359fc5aebeaeab888a5617ea3c245
alex <alex.oliveiramartins05@gmail.com>
2024-10-04 14:19:52 -0300

    added local storage + daily character

--- harrypotterdle/src/components/InputBox.jsx
+++ harrypotterdle/src/components/InputBox.jsx
@@ -17,8 +17,31 @@ export default function InputBox({correctAnswer, playing, setPlaying}) {
         );
         setFilteredNames(filtered);
         setSelectedIndex(0);
-        console.log("selected: ", selectedIndex);
+        // console.log("selected: ", selectedIndex);
     }, [inputValue, characters]);
+
+    // mudar local storage pelas guesses
+    useEffect(() => {
+        if (guesses.length > 0) {
+            const guessNames = guesses.map(guess => guess.name);
+            localStorage.setItem('guesses', JSON.stringify(guessNames));
+        }
+    }, [guesses]);
+
+    useEffect(() => {
+        const savedGuessNames = JSON.parse(localStorage.getItem('guesses') || '[]');
+        const savedGuesses = savedGuessNames.map(name => 
+            characters.find(character => character.name === name)
+        ).filter(Boolean); // Filter out any undefined values
+    
+        setGuesses(savedGuesses);
+        
+        // Remove guessed characters from the available characters
+        const remainingCharacters = characters.filter(character => 
+            !savedGuessNames.includes(character.name)
+        );
+        setCharacters(remainingCharacters);
+    }, []);
 
     // lidar quando o texto muda no inputbox
     const handleInputChange = (e) => {
@@ -54,48 +77,48 @@ export default function InputBox({correctAnswer, playing, setPlaying}) {
     // console.log('InputBox - correctAnswer:', correctAnswer);
 
     return (
-    <div className="w-full space-y-2 relative">
-        {playing && <label htmlFor="UserEmail" className="text-gray-100 block text-2xl font-medium mb-2">
-            Enter character name
-        </label>}
-        <div>
-            {playing && <input
-                type="text"
-                id="UserEmail"
-                value={inputValue}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown}
-                placeholder="Character name, alias"
-                className="w-full px-4 py-3 text-xl rounded-md border-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />}
-            {inputValue.length > 0 && (
-            <ul className='absolute w-full max-w-screen z-20 max-h-screen overflow-y-scroll bg-slate-50'>
-                {filteredNames.map((character, index) => (
-                    <li 
-                    onClick={() => handleClick(character.name)} 
-                    className={`items-center flex space-x-2 flex-row p-2 ${index === selectedIndex ? 'bg-slate-200' : 'hover:bg-slate-100'}`}
-                    key={character.id}
-                    onMouseEnter={() => setSelectedIndex(index)}>
-                        <img className="size-14 rounded-md shadow-sm w-15 h-15" src={character.image}></img>
-                        <span>{character.name}</span>
-                        {character.alias && character.alias !== "" && (
-                            <span className='text-slate-400'>
-                                {` aka.: ${character.alias}`}
-                            </span>
-                        )}
-                    </li>
+        <div className="w-full space-y-2 relative">
+            {playing && <label htmlFor="UserEmail" className="text-gray-100 block text-2xl font-medium mb-2">
+                Enter character name
+            </label>}
+            <div>
+                {playing && <input
+                    type="text"
+                    id="UserEmail"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Character name, alias"
+                    className="w-full px-4 py-3 text-xl rounded-md border-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />}
+                {inputValue.length > 0 && (
+                <ul className='absolute w-full max-w-screen z-20 max-h-screen overflow-y-scroll bg-slate-50'>
+                    {filteredNames.map((character, index) => (
+                        <li 
+                        onClick={() => handleClick(character.name)} 
+                        className={`items-center flex space-x-2 flex-row p-2 ${index === selectedIndex ? 'bg-slate-200' : 'hover:bg-slate-100'}`}
+                        key={`${character.id}-${index}`}
+                        onMouseEnter={() => setSelectedIndex(index)}>
+                            <img className="size-14 rounded-md shadow-sm w-15 h-15" src={character.image}></img>
+                            <span>{character.name}</span>
+                            {character.alias && character.alias !== "" && (
+                                <span className='text-slate-400'>
+                                    {` aka.: ${character.alias}`}
+                                </span>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+                )}
+            </div>
+            <div className='flex flex-col overflow-x-auto'>
+                <AtributesTitle/>
+                <div className='space-y-2'>
+                {guesses.map((guess, index) => (
+                    <Guess key={index} setPlaying={setPlaying} character={guess} correctAnswer={correctAnswer} />
                 ))}
-            </ul>
-            )}
-        </div>
-        <div className='flex flex-col overflow-x-auto'>
-            <AtributesTitle/>
-            <div className='space-y-2'>
-            {guesses.map((guess, index) => (
-                <Guess key={index} setPlaying={setPlaying} character={guess} correctAnswer={correctAnswer} />
-            ))}
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
--- harrypotterdle/src/pages/Home.jsx
+++ harrypotterdle/src/pages/Home.jsx
@@ -6,21 +6,38 @@ import { useEffect } from "react";
 import Wallpaper from '../assets/Wallpaper.webp';
 import Answer from '../components/Answer';
 
+
+
 function Home() {
   const [correctAnswer, setCorrectAnswer] = useState(null);
   const [playing, setPlaying] = useState(true);
+  const [date, setDate] = useState(localStorage.getItem('date'));
 
   useEffect(() => {
-    console.log("you won!!");
-    
-  }, [playing]);
-  
-  useEffect(() => {
-    const randomCharacter = characterData[Math.floor(Math.random() * characterData.length)];
-    setCorrectAnswer(randomCharacter);
+    const today = new Date();
+    today.setDate(today.getDate())
+
+    const todayString = today.getDate().toString();
+    const storedDate = localStorage.getItem('date');
+
+    // console.log("Today's date:", todayString);
+    // console.log("Stored date:", storedDate);
+
+    if (storedDate !== todayString) {
+      // console.log("Date has changed, clearing local storage")
+      localStorage.removeItem('guesses');
+      localStorage.setItem('date', todayString);
+      setDate(todayString);
+    } else {
+      setDate(storedDate);
+    }
+
+    const seed = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+    const randomIndex = seed % characterData.length;
+    setCorrectAnswer(characterData[randomIndex]);
   }, []);
 
-  console.log(correctAnswer);
+  // console.log(correctAnswer);
 
   return (
     <div className="w-screen h-screen bg-cover overflow-y-auto" style={{ backgroundImage: `url(${Wallpaper})` }}>
@@ -30,12 +47,12 @@ function Home() {
         {correctAnswer && <InputBox setPlaying={setPlaying} playing={playing} correctAnswer={correctAnswer} />}
       </div>
       <div 
-      class="opacity-75 min-w-max flex font-light flex-row items-center space-x-1 justify-center text-gray-50">
-        <footer class="">made by
+      className="opacity-75 min-w-max flex font-light flex-row items-center space-x-1 justify-center text-gray-50">
+        <footer>made by
         </footer>
         <a 
           href="https://github.com/alexoliveiramartins" 
-          class="font-semibold decoration-dotted underline">
+          className="font-semibold decoration-dotted underline">
             alex
           </a>
       </div>
